1f4cca7a5e3fde677cb29aad6348bb99a195d826
11 <leeroycunha@gmail.com>
2023-08-13 11:08:13 +0200

    fixed issue where / stopped text output

--- zip.php
+++ zip.php
@@ -9,18 +9,22 @@ $zipFileName = 'my-archive.zip';
 
 $sourceFilePath = 'files-to-zip/motivatie.txt'; // Relative path to the file
 
+// Read the file content with explicit encoding and line endings
+$fileContent = file_get_contents($sourceFilePath, false, null, 0, null);
+$fileContent = str_replace("\r\n", "\n", $fileContent); // Convert Windows line endings to Unix
+
 try {
     if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE)) {
         // Add the motivatie.txt file to the zip archive using LZMA compression
         $archiveFilePath = 'motivatie.txt'; // This is the path within the archive
 
-        if ($zip->addFile($sourceFilePath, $archiveFilePath, ZipArchive::CM_LZMA)) {
+        if ($zip->addFromString($archiveFilePath, $fileContent)) {
             // Close the zip archive
             $zip->close();
 
             echo "Zip archive created successfully.";
 
-            // Move the zip archive to either the 'files-to-zip' or 'php-zip' directory
+            // Move the zip archive to either the 'files-to-zip' directory
             $targetDirectory = 'zip-or-obtain'; 
             $targetPath = $targetDirectory . '/' . $zipFileName;
 
@@ -38,4 +42,3 @@ try {
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
-?>
